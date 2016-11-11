da137f6d9220d7b7312f57fde362c471ee6d8bfd
Ben Christel <bchristel@pivotal.io>
2016-11-10 21:38:50 -0800

    Script runner prevents the evaled script from accessing external variables

--- script-runner.js
+++ script-runner.js
@@ -1,9 +1,18 @@
+"use strict";
+
 window.addEventListener('message', function(event) {
   var BadOSExtensionOrigin =
     'chrome-extension://behahjpncgjkcbcnkngledgbikppjbhi'
 
   if (event.origin === BadOSExtensionOrigin) {
-    var result = eval(event.data.script)
+    var varsToCloak = [
+      'event',
+      'BadOSExtensionOrigin',
+      'varsToCloak',
+      'result',
+      '__scriptRunnerUtil__'
+    ]
+    var result = eval(__scriptRunnerUtil__.cloakVars(event.data.script, varsToCloak))
 
     function render(lines) {
       console.log('called render')
@@ -13,3 +22,25 @@ window.addEventListener('message', function(event) {
     }
   }
 })
+
+var __scriptRunnerUtil__ = {}
+
+__scriptRunnerUtil__.cloakVars = function(script, extraVars) {
+  var iife = __scriptRunnerUtil__.iife
+  var defineAsUndefined = __scriptRunnerUtil__.defineAsUndefined
+  var cloak = extraVars
+    .concat(Object.keys(window))
+    .concat(['window'])
+    .map(defineAsUndefined)
+    .join('')
+
+  return iife(cloak + iife(script))
+}
+
+__scriptRunnerUtil__.iife = function(script) {
+  return ';(function() {' + script + '})();'
+}
+
+__scriptRunnerUtil__.defineAsUndefined = function(varName) {
+  return 'var ' + varName + ' = undefined;'
+}
