263929cb2f10e4129eb38e8e373cbd0a56441a9b
Ben Christel <bchristel@pivotal.io>
2016-11-11 21:48:11 -0800

    Remove the check for message origin
    - I don't think this is a security vulnerability in a chrome app

--- motherboard.js
+++ motherboard.js
@@ -1,42 +1,38 @@
 "use strict";
 
-var BadOSExtensionOrigin =
-  'chrome-extension://behahjpncgjkcbcnkngledgbikppjbhi'
-
 var __scriptRunnerUtil__ = {}
 
 __scriptRunnerUtil__.boot = function(event) {
+  console.log(event)
   if (!event.data.script) return
+  console.log(event.origin)
 
-  if (event.origin === BadOSExtensionOrigin) {
-    var varsToCloak = [
-      'window',
-      'BadOSExtensionOrigin',
-      '__scriptRunnerUtil__',
-      'event',
-      'varsToCloak'
-    ].concat(Object.keys(window))
+  var varsToCloak = [
+    'window',
+    '__scriptRunnerUtil__',
+    'event',
+    'varsToCloak'
+  ].concat(Object.keys(window))
 
-    // render is intentionally exposed to the eval'd script.
-    // don't cloak it!
-    function render(lines) {
-      console.log('called render')
-      event.source.postMessage({
-        lines: lines
-      }, BadOSExtensionOrigin)
-    }
-
-    // onMessage is intentionally exposed to the eval'd script.
-    // don't cloak it!
-    function onMessage(callback) {
-      window.addEventListener('message', callback)
-    }
-
-    eval(__scriptRunnerUtil__.cloakVars(event.data.script, varsToCloak))
-
-    // ensure we only boot once
-    window.removeEventListener('message', __scriptRunnerUtil__.boot)
+  // render is intentionally exposed to the eval'd script.
+  // don't cloak it!
+  function render(lines) {
+    console.log('called render')
+    event.source.postMessage({
+      lines: lines
+    }, '*')
   }
+
+  // onMessage is intentionally exposed to the eval'd script.
+  // don't cloak it!
+  function onMessage(callback) {
+    window.addEventListener('message', callback)
+  }
+
+  eval(__scriptRunnerUtil__.cloakVars(event.data.script, varsToCloak))
+
+  // ensure we only boot once
+  window.removeEventListener('message', __scriptRunnerUtil__.boot)
 }
 
 window.addEventListener('message', __scriptRunnerUtil__.boot)
